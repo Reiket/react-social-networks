59bb9632f78569911a798c005ae1a47f5ac6f600
Reiket <olinyko2607@gmail.com>
2023-05-18 14:52:47 +0300

    Updates

---
+++ static/js/120.3fde2dd3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[120],{7562:function(e,n,u){u.r(n);var p=u(184);n.default=function(e){return(0,p.jsx)("div",{children:"News"})}}}]);
+//# sourceMappingURL=120.3fde2dd3.chunk.js.map
